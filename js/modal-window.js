17a8a39d16cb1f11eb0461752ca3c70f42e35398
Adina Halter <haltersweb@gmail.com>
2015-07-18 18:18:34 -0400

    added a css file for modal-window.  Fleshed out initial commands to block screen.

--- js/modal-window.js
+++ js/modal-window.js
@@ -7,7 +7,7 @@
     /*
     showing the overlay:
         1. flag trigger used to open overlay
-        2. show blocker screen
+        2. show screen blocker
         3. block page content
         4. hide page content from reader
         5. unblock overlay content from screen reader
@@ -27,24 +27,54 @@
     /*
         assign click event to open the modal window
     */
-    $('[data-trigger="modal"]').on('click', function (evt) {
+    $('[data-controls="modal"]').on('click', function (evt) {
         var $modalTrigger = $(this),
             $blockScreen = $('.block-screen'),
+            $blockedContainers = $('.page-wrapper'),
             $modal = $('#' + $modalTrigger.attr('aria-controls')),
-            $blockedContainers=$('.page-wrapper'),
-            $closeModalBtn=$modal.find('.close');
+            $closeModalBtn = $modal.find('[data-control="close"]');
+        //flag trigger used to open overlay
         NAME.access.tagTrigger();
+        //show screen blocker
+        $blockScreen.addClass('active');
+        //block focus in blocked containers
+        //hide page content from reader
         NAME.access.blockFocus($blockedContainers, $closeModalBtn);
-
-        openModal();
+        //unblock overlay content from screen reader
+        //position and show overlay
+        //bind esc to trigger close button
+        //put focus on close button.
+        openDialog();
     });
+    $('[data-widget="modal"] [data-control="close"]').on('click', function (evt) {
+        var $closeModalBtn = $(this),
+            $blockScreen = $('.block-screen'),
+            $blockedContainers=$('.page-wrapper');
 
+        closeDialog();
+        NAME.access.removeBlockFocus($blockedContainers);
+        NAME.access.focusTrigger();
+    });
     /*
-        function to open a subnav
+        function to open a dialog box
     */
-    function openModal($nav, $subnav) {
-        //NAME.access.ariaExpand($subnav);
+    function openDialog($dialog) {
+        /*
+        1. set aria-hidden = false;
+        2. position dialog (is this a tooltip or a modal?)
+        3. show dialog
+        */
         console.log('openModal function run.');
+    }
+    /*
+        function to close a dialog box
+    */
+    function closeDialog() {
+        /*
+        1. set aria-hidden = true;
+        2. hide dialog;
+        */
+        console.log('closeModal function run.');
     }
 
 }(jQuery, NAME));
