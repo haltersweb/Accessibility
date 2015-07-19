f71322e814437ab34d6464b60aaeeeb2e4f5542b
Adina Halter <haltersweb@gmail.com>
2015-07-18 23:55:14 -0400

    reconfigured to put more into function and less directly into click event. added bookends

--- js/modal-window.js
+++ js/modal-window.js
@@ -24,55 +24,110 @@
         7. remove flag
     */
 
+    var $currentDialog = null, // a reference to the dialog box to be shown
+        $triggerElement = null, // a reference to the element that triggered the dialog box
+        $screenBlock, // a reference to the dimmed layer that blocks what's behind it.
+        $contentsToBlock; // a reference to content containers that are blocked when dialog is visible.
     /*
         assign click event to open the modal window
     */
     $('[data-controls="modal"]').on('click', function (evt) {
-        var $modalTrigger = $(this),
-            $blockScreen = $('.block-screen'),
-            $blockedContainers = $('.page-wrapper'),
-            $modal = $('#' + $modalTrigger.attr('aria-controls'));
-        //flag trigger used to open overlay
-        NAME.access.tagTrigger();
-        //show screen blocker
-        $blockScreen.addClass('active');
+        // identify the modal
+        $currentDialog = $('#' + $(this).attr('aria-controls'));
+        // identify what content containers will be blocked when the modal is active
+        $contentsToBlock = $('.page-wrapper');
         //unblock overlay content from screen reader
         //position and show overlay
         //bind esc to trigger close button
         //put focus on close button.
-        openDialog($modal, "modal", $blockedContainers);
+        showDialog($currentDialog, $contentsToBlock);
     });
     $('[data-widget="modal"] [data-control="close"]').on('click', function (evt) {
         var $closeModalBtn = $(this),
-            $blockScreen = $('.block-screen'),
-            $blockedContainers=$('.page-wrapper');
+            $screenBlock = $('.block-screen'),
+            $contentsToBlock=$('.page-wrapper');
 
         closeDialog();
         NAME.access.removeBlockFocus($blockedContainers);
         NAME.access.focusTrigger();
     });
+    function showDialog($dialog, $blockedContainers) {
+        var $firstActionableElement = $dialog.find(NAME.focusables).not('[data-bookends]').eq(0);
+        // if there is no actionable element inside dialog, make the dialog the actionable element
+        if ($firstActionableElement.length === 0) {
+            $dialog.attr('tabindex', '0');
+            $firstActionableElement = $dialog;
+        }
+        //if $blockedContainers has been passed into the function then this is a modal
+        if ($blockedContainers) {
+            // identify the element that blocks the content
+            $screenBlock = $('.block-screen');
+            //show screen blocker
+            $screenBlock.addClass('active');
+            //add bookends to modal
+            addBookends($dialog);
+            //constrain tabbing to bookends
+            bookendFocus($dialog);
+            //position in center
+            positionModal();
+            //block focus in blocked containers & hide page content from reader
+            NAME.access.blockFocus($blockedContainers, $firstActionableElement);
+        // else this is a tooltip
+        } else {
+            //position relative to trigger
+        }
+        // flag the trigger used to open dialog
+        NAME.access.tagTrigger();
+        // reveal dialog content to screen reader
+        $dialog.attr("aria-hidden", "false");
+        // show the dialog box
+        $dialog.addClass('active');
+        //bind esc to close the dialog box
+        $dialog.on('keyup.esc-key', function (event) {
+            escapeKeyClose(event);
+        });
+        //put focus on close button
+        $firstActionableElement.focus();
+        console.log('openModal function run.');
+    }
+    function addBookends($container) {
+        var bookendMarkup = '<div tabindex="0" data-bookends></div>';
+        //first check to see if bookends already exists
+        if($('[data-bookends]').length > 0) {
+            return true;
+        }
+        //add bookends
+        $container.prepend(bookendMarkup);
+        $container.append(bookendMarkup);
+    }
+    function bookendFocus($container) {
+        // Capture focus events on bookends to contain tabbing within a container
+        // Note: We don't need to know the direction, because the only way to
+        // tab to the first bookend is to shift tab (backwards), and
+        // the only way to tab to the last bookend is to tab (forwards).
+        var $bookends = $container.find('[data-bookends]');
+        // First check to make sure the bookends exist
+        if ($bookends.size() > 0) {
+            // Focusing first bookend sends focus to last real focusable element
+            $bookends.eq(0).on('focus.bookend-focus', function (event) {
+                var $focusableElements = $container.find(NAME.focusables);
+                $focusableElements.eq(-2).focus();
+            });
+            // Focusing last bookend sends focus to the first real focusable element
+            $bookends.eq(1).on('focus.bookend-focus', function (event) {
+                var $focusableElements = $container.find(NAME.focusables);
+                $focusableElements.eq(1).focus();
+            });
+        }
+    }
+    function positionModal() {
+
+    }
     /*
         function to open a dialog box
     */
     function openDialog($dialog, modalOrTooltip, $blockedContainers) {
         var $closeModalBtn = $dialog.find('[data-control="close"]');
-        $dialog.attr("aria-hidden", "false");
-        if (modalOrTooltip = "modal") {
-            //position in center
-            //block focus in blocked containers & hide page content from reader
-            NAME.access.blockFocus($blockedContainers, $closeModalBtn);
-        }
-        if (modalOrTooltip = "tooltip") {
-            //position relative to [data-trigger="true"]
-        }
-        $dialog.addClass('active');
-        //bind esc to trigger close button
-        $dialog.on('keyup.esc-key-closes-dialog', function (event) {
-            escapeKeyClose(event);
-        });
-        //put focus on close button
-        $closeModalBtn.focus();
-        console.log('openModal function run.');
     }
     /*
         esc key closes dialog
@@ -86,15 +141,15 @@
                 return;
             }
             // Close the current overlay
-            if ($currentOverlay) {
-                hideOverlay($currentOverlay);
+            if ($currentDialog) {
+                hideDialog($currentDialog);
             }
         }
     }
     /*
         function to close a dialog box
     */
-    function closeDialog() {
+    function hideDialog($dialog) {
         /*
         1. set aria-hidden = true;
         2. hide dialog;
