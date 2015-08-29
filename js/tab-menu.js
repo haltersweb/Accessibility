da9e14cc31019b28541c79d948015ba05583a46c
Adina Halter <haltersweb@gmail.com>
2015-08-29 16:40:43 -0400

    tab menu examples

---
+++ js/tab-menu.js
@@ -0,0 +1,25 @@
+/**
+ * @author Adina Halter
+ * Accessible Tab Navigation
+ */
+(function ($, NAME) {
+    'use strict';
+    $('[data-widget="tab-menu"]').find('a').on('click', function (evt) {
+    	var $this = $(this),
+    		$targetContent;
+    	$this.closest('[data-widget="tab-menu"').find('a').removeClass('current');
+    	$this.addClass('current');
+    	$targetContent = $($this.attr('href'));
+    	$targetContent.siblings('.content-panel').removeClass('current');
+    	$targetContent.addClass('current').focus();
+    });
+    /*
+		toggle between single content panel view and full content view
+    */
+    $('#singleContainerView').on('click', function (evt) {
+    	$('.tabbed-content').addClass('single-container-view');
+    });
+    $('#allContentView').on('click', function (evt) {
+    	$('.tabbed-content').removeClass('single-container-view');
+    });
+}(jQuery, NAME));
