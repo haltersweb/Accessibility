3d3f7965bcaff0560a8dc85136545e298cd491f4
Adina Halter <haltersweb@gmail.com>
2015-10-06 08:55:54 -0400

    added a blind and changed the text

--- js/find-the-bugs.js
+++ js/find-the-bugs.js
@@ -4,8 +4,11 @@
  */
 (function ($, NAME) {
     'use strict';
-    $('a.btn, button').on('click', function (evt) {
+    $('a.btn').on('click', function (evt) {
         evt.preventDefault();
-        alert("You clicked me!!");
+        alert("Well, go ahead and have a lie down, then.");
+    });
+    $('.blind-trigger').on('click', function (evt) {
+        $('.blind-container').toggleClass('accessibly-hidden');
     });
 }(jQuery, NAME));
