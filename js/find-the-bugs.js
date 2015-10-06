f48d0db008d77675c0cbd925a1f99d12a57ac99a
Adina Halter <haltersweb@gmail.com>
2015-10-06 08:31:17 -0400

    find the bug exercise

---
+++ js/find-the-bugs.js
@@ -0,0 +1,10 @@
+/**
+ * @author Adina Halter
+ * Accessible Tab Navigation
+ */
+(function ($, NAME) {
+    'use strict';
+    $('a.btn, button').on('click', function (evt) {
+        alert("You clicked me!!");
+    });
+}(jQuery, NAME));
