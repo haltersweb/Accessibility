cfdf3bd96ae65f2f00c4008a9e0279f167817dc3
Adina Halter <haltersweb@gmail.com>
2015-12-17 10:10:57 -0500

    testing icon labeling

---
+++ js/coloring-table.js
@@ -0,0 +1,18 @@
+(function ($) {
+    'use strict';
+    /*
+
+    */
+    NAME.colorTableContent = function (tableId, text, color, boldBoolean) {
+    	var $cells = $('#' + tableId).find('td'),
+    		fontWeight = boldBoolean ? "bold" : "normal";
+    	$cells.each(function (i) {
+    		if ($(this).text() === text) {
+    			$(this).css({
+    				"color" : color,
+    				"font-weight" : fontWeight
+    			})
+    		}
+    	})
+    };
+}(jQuery));
