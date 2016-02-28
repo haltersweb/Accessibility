bc64d39630223d3d3685f6da1537e131ba64d5ee
Adina Halter <haltersweb@gmail.com>
2016-02-27 23:28:44 -0500

    sample of stream json

---
+++ js/stream-json.js
@@ -0,0 +1,37 @@
+{
+	"response": {
+		"players": [{
+			"steamid": "76561197960435530",
+			"communityvisibilitystate": 3,
+			"profilestate": 1,
+			"personaname": "Robin",
+			"lastlogoff": 1456632658,
+			"profileurl": "http://steamcommunity.com/id/robinwalker/",
+			"avatar": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f1/f1dd60a188883caf82d0cbfccfe6aba0af1732d4.jpg",
+			"avatarmedium": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f1/f1dd60a188883caf82d0cbfccfe6aba0af1732d4_medium.jpg",
+			"avatarfull": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/f1/f1dd60a188883caf82d0cbfccfe6aba0af1732d4_full.jpg",
+			"personastate": 0,
+			"realname": "Robin Walker",
+			"primaryclanid": "103582791429521412",
+			"timecreated": 1063407589,
+			"personastateflags": 0,
+			"loccountrycode": "US",
+			"locstatecode": "WA",
+			"loccityid": 3961
+		}, {
+			"steamid": "76561198152299810",
+			"communityvisibilitystate": 3,
+			"personaname": "haltersweb",
+			"lastlogoff": 1454945780,
+			"profileurl": "http://steamcommunity.com/profiles/76561198152299810/",
+			"avatar": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb.jpg",
+			"avatarmedium": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_medium.jpg",
+			"avatarfull": "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/fe/fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb_full.jpg",
+			"personastate": 0,
+			"primaryclanid": "103582791429521408",
+			"timecreated": 1409341402,
+			"personastateflags": 0
+		}]
+
+	}
+}
