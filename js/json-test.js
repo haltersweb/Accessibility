b9fe8932835c0532f1f906ec8023240223368640
Adina Halter <haltersweb@gmail.com>
2016-02-27 23:33:54 -0500

    testing local version of xml from stream

--- js/json-test.js
+++ js/json-test.js
@@ -1,3 +1,35 @@
-$.getJSON("js/some-json.js",function(data){
+/*
+$.ajax({
+  url: "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=AB5F2B2D478D8FDC601701509B768B61&steamids=76561197960435530,76561198152299810",
+
+ dataType: "jsonp",
+ jsonp: false,
+  success: function(data){
+  	console.log(data);
+  }
+});
+
+$.ajax({
+  url: "http://json2jsonp.com/?http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=AB5F2B2D478D8FDC601701509B768B61&steamids=76561197960435530,76561198152299810&callback=?",
+
+ dataType: "jsonp",
+ jsonp: false,
+  success: function(data){
     console.log(data);
+  }
+});
+*/
+
+/*
+$.get( "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=AB5F2B2D478D8FDC601701509B768B61&steamids=76561197960435530,76561198152299810", function( data ) {
+  alert( "Load was performed." );
+});
+*/
+
+$.get( "js/some-json.js", function( data ) {
+  alert( "Load was performed." );
+});
+
+$.get( "js/stream-json.js", function( data ) {
+  console.log(data.response.players);
 });
